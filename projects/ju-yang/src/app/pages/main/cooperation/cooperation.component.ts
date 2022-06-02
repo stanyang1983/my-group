aedc2511a86ce5bd9ebf0db6cb08e652caad0f86
陽雲豪 <stanyang1983@gmail.com>
2022-06-02 23:09:43 +0800

    更新banner add

--- projects/ju-yang/src/app/pages/main/cooperation/cooperation.component.ts
+++ projects/ju-yang/src/app/pages/main/cooperation/cooperation.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cooperation',
   templateUrl: './cooperation.component.html',
-  styleUrls: ['./cooperation.component.scss']
+  styleUrls: ['./cooperation.component.scss', '../../stylesAll.scss']
 })
 export class CooperationComponent implements OnInit {
 
--- projects/ju-yang/src/app/pages/main/quality/quality.component.ts
+++ projects/ju-yang/src/app/pages/main/quality/quality.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-quality',
   templateUrl: './quality.component.html',
-  styleUrls: ['./quality.component.scss']
+  styleUrls: ['./quality.component.scss', '../../stylesAll.scss']
 })
 export class QualityComponent implements OnInit {
 
